e80d16fcae4aa7332587577d00f4abd0ebca02ef
Chris Sosnin <48099298+slumber@users.noreply.github.com>
2023-07-25 02:55:02 +0300

    introduce r1cs primitives (#1)
    * introduce r1cs primitives
    
    * Improve commitment code
    
    * fix randomness in commitments
    
    * commitment to cross-term
    
    * Implement R1CS folding
    
    * fix satisfiability
    
    * fix ci
    
    * Drop randomness from commitment scheme
    
    * tests
    
    * clippy
    
    * fmt

--- supernova/src/pedersen.rs
+++ supernova/src/pedersen.rs
@@ -1,60 +1,53 @@
+use std::marker::PhantomData;
+
 use crate::commitment::CommitmentScheme;
-use ark_ec::{Group, VariableBaseMSM};
-use ark_std::UniformRand;
-use ark_test_curves::bls12_381::{G1Affine as GAffine, G1Projective as G};
+use ark_ec::{Group, ScalarMul, VariableBaseMSM};
 
-pub struct PedersenCommitment {}
+pub struct PedersenCommitment<G>(PhantomData<G>);
 
-// TODO: This should be generic over any group
-impl CommitmentScheme<G> for PedersenCommitment {
-    // (g, h)
-    type PP = (Vec<G>, G);
+impl<G: VariableBaseMSM> CommitmentScheme<G> for PedersenCommitment<G> {
+    type PP = Vec<G>;
+
+    type Commitment = G;
 
     fn setup(n: usize) -> Self::PP {
+        // TODO: replace test rng.
         let mut rng = ark_std::test_rng();
-        let h = G::rand(&mut rng);
-        let g = (0..n).map(|_| G::rand(&mut rng)).collect();
-
-        (g, h)
+        (0..n).map(|_| G::rand(&mut rng)).collect()
     }
 
-    fn commit(pp: &Self::PP, x: &[<G as Group>::ScalarField], r: <G as Group>::ScalarField) -> G {
-        let gs = &pp.0.iter().map(|x| (*x).into()).collect::<Vec<GAffine>>()[..];
-        let h = pp.1.into();
-        let bases = &[&[h], gs].concat()[..];
-        let scalars = &[&[r], x].concat();
-        G::msm(bases, scalars).unwrap()
+    fn commit(pp: &Self::PP, x: &[G::ScalarField]) -> G {
+        let h = ScalarMul::batch_convert_to_mul_base(pp);
+        let bases = &h;
+        let scalars = x;
+        VariableBaseMSM::msm_unchecked(bases, scalars)
     }
 
-    fn open(
-        pp: &Self::PP,
-        c: G,
-        x: &[<G as Group>::ScalarField],
-        r: <G as Group>::ScalarField,
-    ) -> bool {
-        Self::commit(pp, x, r) == c
+    fn open(pp: &Self::PP, c: G, x: &[<G as Group>::ScalarField]) -> bool {
+        Self::commit(pp, x) == c
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use ark_std::UniformRand;
     use ark_test_curves::bls12_381::Fr as ScalarField;
+    use ark_test_curves::bls12_381::G1Projective as G;
 
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn commitment_matches() {
         let n = 3;
         let mut rng = ark_std::test_rng();
 
         let x = &(0..n)
             .map(|_| ScalarField::rand(&mut rng))
             .collect::<Vec<ScalarField>>()[..];
-        let r = ScalarField::rand(&mut rng);
 
-        let pp = PedersenCommitment::setup(n);
-        let c = PedersenCommitment::commit(&pp, x, r);
-        let res = PedersenCommitment::open(&pp, c, x, r);
+        let pp = PedersenCommitment::<G>::setup(n);
+        let c = PedersenCommitment::<G>::commit(&pp, x);
+        let res = PedersenCommitment::<G>::open(&pp, c, x);
 
         assert!(res)
     }
