36e8a06bb9feb188dbcb924ee55796e291afcfcd
Paul Govereau <paul@govereau.com>
2023-09-11 17:06:01 -0400

    implement nexus-run

--- nexus-tools/src/nexus-run.rs
+++ nexus-tools/src/nexus-run.rs
@@ -1,3 +1,21 @@
+use nexus_riscv::*;
+use std::env;
+use std::path::PathBuf;
+use std::process;
+
 fn main() {
-    println!("Nexus-run is not yet implemented");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Usage nexus-run <elf_file>");
+        process::exit(1);
+    }
+
+    let path = &PathBuf::from(args[1].clone());
+    match run_elf(path, false) {
+        Ok(()) => (),
+        Err(e) => {
+            println!("{e}");
+            process::exit(1)
+        }
+    }
 }
