98e5f8a47322241312441fca62bb5a29f4ca1c80
Paul Govereau <paul@govereau.com>
2023-10-23 12:11:43 -0400

    add github CI config

---
+++ bin/pre-commit.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# This script runs the same checks as the github CI;
+#  it can be used a a pre-commit hook.
+
+set -e
+
+cargo fmt --all --check
+cargo build
+cargo clippy --all-targets
+cargo test
