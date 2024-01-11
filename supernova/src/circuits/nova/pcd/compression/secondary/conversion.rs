de0c192002cb436c337e5ba169c167a307cb9b7f
Dan Dore <dorebell@gmail.com>
2024-01-11 02:08:33 -0800

    conversion from ark_ff::PrimeField to ff::PrimeField

--- supernova/src/circuits/nova/pcd/compression/secondary/conversion.rs
+++ supernova/src/circuits/nova/pcd/compression/secondary/conversion.rs
@@ -1,40 +1,82 @@
-use ark_ec::{
-    short_weirstrass::{Projective, SWCurveConfig},
-    CurveGroup,
-};
-use ark_ff::{Field as ArkField, PrimeField};
-use halo2_proofs::arithmetic::Field as FfField;
+use ark_ff::{BigInteger, PrimeField as ArkPrimeField};
+use ff::{FromUniformBytes, PrimeField as FfPrimeField};
 
-pub struct FieldContainer<F: ArkField>(F);
-
-impl<F: ArkField> Into<FieldContainer<F>> for F {
-    fn into(self) -> FieldContainer<F> {
-        FieldContainer(self)
+#[derive(Debug)]
+pub enum FieldConversionError {
+    ModulusWrongSize,
+    ModulusMismatch,
+    #[cfg(test)]
+    ValueMismatch,
+}
+/// converts an arkworks prime field element to a ff prime field element, generically in the
+/// fields. The const parameter `N` is specified by the impl of `ff::FromUniformBytes` for the
+/// struct implementing `ff::PrimeField`. For the fields in `halo2curves::grumpkin`, this is `64`.
+/// This function returns an error if the moduli of the two fields do not match.
+#[allow(dead_code)]
+pub fn ark_to_ff<AF, FF, const N: usize>(x: AF) -> Result<FF, FieldConversionError>
+where
+    AF: ArkPrimeField,
+    FF: FfPrimeField + FromUniformBytes<N>,
+{
+    // check that the size of the moduli for the two fields match
+    if AF::MODULUS_BIT_SIZE != FF::NUM_BITS {
+        return Err(FieldConversionError::ModulusWrongSize);
     }
+
+    // check that the moduli for the two fields match
+    let ark_modulus_bytes = AF::MODULUS.to_bytes_be();
+    // this is a bit of an ugly hack, but the ff PrimeField trait only provides a
+    // string representation of the modulus. This might not work for all fields,
+    // but it works for the grumpkin fields.
+    if format!("0x{}", hex::encode(ark_modulus_bytes)) != FF::MODULUS {
+        return Err(FieldConversionError::ModulusMismatch);
+    }
+
+    // ff requires N to be larger than the number of bytes in the modulus + 16,
+    // so the above check should ensure this does not fail
+    let mut le_bytes = [0u8; N];
+    let x_le_bytes = x.into_bigint().to_bytes_le();
+    // this should never fail, since we already checked that the modulus bit sizes match,
+    // and `ff::FromUniformBytes`` requires N to be larger than the number of bytes in the modulus.
+    assert!(x_le_bytes.len() <= N);
+    le_bytes[..x_le_bytes.len()].copy_from_slice(&x_le_bytes);
+
+    Ok(FF::from_uniform_bytes(&le_bytes))
 }
 
-impl<F: ArkField> FfField for FieldConvContainer<F> {
-    const ZERO: Self = Self(F::zero());
-    const ONE: Self = Self(F::one());
+#[cfg(test)]
+mod tests {
+    use ark_grumpkin::{Fq as ArkFq, Fr as ArkFr};
+    use ark_std::test_rng;
+    use halo2curves::grumpkin::Fr as FfFr;
+    use rand_core::RngCore;
 
-    fn random(rng: impl RngCore) -> Self {
-        Self(F::random(rng))
+    use super::*;
+
+    fn field_conversion_test_helper<AF, FF, const N: usize>() -> Result<(), FieldConversionError>
+    where
+        AF: ArkPrimeField,
+        FF: FfPrimeField + FromUniformBytes<N>,
+    {
+        let mut rng = test_rng();
+        let mut bytes = [0u8; N];
+        rng.fill_bytes(&mut bytes);
+        let x_ark = AF::from_le_bytes_mod_order(bytes.as_slice());
+        let x_ff = FF::from_uniform_bytes(&bytes);
+        let x_conv = ark_to_ff::<AF, FF, N>(x_ark)?;
+        if x_ff != x_conv {
+            return Err(FieldConversionError::ValueMismatch);
+        }
+        Ok(())
     }
 
-    fn square(&self) -> Self {
-        todo!()
+    #[test]
+    fn field_conversion_test() {
+        field_conversion_test_helper::<ArkFr, FfFr, 64>().unwrap();
     }
 
-    fn double(&self) -> Self {
-        todo!()
+    #[test]
+    fn field_conversion_test_wrong_modulus() {
+        field_conversion_test_helper::<ArkFq, FfFr, 64>().unwrap_err();
     }
-
-    fn invert(&self) -> CtOption<Self> {
-        todo!()
-    }
-
-    fn sqrt_ratio(num: &Self, div: &Self) -> (Choice, Self) {
-        todo!()
-    }
-    
 }
