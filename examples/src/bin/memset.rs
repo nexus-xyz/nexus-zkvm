d8e342e1a8146b4d04c7ff67b5ec9a4f19aab07c
Samuel Judson <sam@sjudson.com>
2024-04-18 14:38:40 -0400

    Fix optimization.

--- examples/src/bin/memset.rs
+++ examples/src/bin/memset.rs
@@ -4,12 +4,13 @@
 extern crate alloc;
 use alloc::vec;
 
-use nexus_rt::write_log;
+use nexus_rt::{Write, println};
 
 // use 16mb
 // alternatively, remove memset argument or otherwise set less than (slightly more than) 6mb and it will fail
 #[nexus_rt::main(memset(16))]
 fn main() {
-    let _ = vec![0 as u32; 1500000];
-    write_log("Success!!!\n");
+    let vec = vec![0 as u32; 1500000];
+    println!("Arbitrary vector entry so that it does not get optimized away: vec[42] = {}", vec[42]);
+    println!("Success!!!");
 }
