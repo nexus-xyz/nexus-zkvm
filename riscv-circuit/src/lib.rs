814ba2fdf0d71626417815fb2d70949b630ad3e2
Samuel Judson <sam@sjudson.com>
2024-05-08 13:58:14 -0400

    Restore old circuits.

---
+++ riscv-circuit/src/lib.rs
@@ -0,0 +1,40 @@
+#![allow(non_snake_case)]
+
+//! Circuits for the Nexus RISC-V virtual machine
+
+pub mod r1cs;
+pub mod riscv;
+
+use r1cs::*;
+use riscv::*;
+
+use nexus_riscv::{Result, nop_vm, vm::eval::*, vm::trace::*};
+
+pub use nexus_riscv::vm::trace::Trace;
+
+pub fn step_circuit() -> Result<R1CS> {
+    // use unimp instruction as nop
+    let mut vm = nop_vm(0);
+    let mut tr = trace(&mut vm, 1, false)?;
+    let b0 = tr.blocks.remove(0);
+    let w = b0.into_iter().next().unwrap();
+    let cs = big_step(&w, false);
+    Ok(cs)
+}
+
+pub fn eval(vm: &mut VM, k: usize, check: bool) -> Result<Trace> {
+    let tr = trace(vm, k, false)?;
+    if check {
+        let mut cs = step_circuit()?;
+        for b in &tr.blocks {
+            println!("checking block...");
+            for w in b {
+                println!("checking step...");
+                let cs2 = big_step(&w, false);
+                cs.w = cs2.w;
+                assert!(cs.is_sat());
+            }
+        }
+    }
+    Ok(tr)
+}
