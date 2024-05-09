249e609f70fd163e9bdf74a68a86ffbfb0217a7d
Samuel Judson <sam@sjudson.com>
2024-05-09 11:36:08 -0400

    Move everything together, without the pieces tied up internally.

--- vm/src/eval.rs
+++ vm/src/eval.rs
@@ -1,179 +1,201 @@
-//! Evaluation for Nexus VM programs.
+//! A Virtual Machine for RISC-V
 
-use num_traits::FromPrimitive;
+use nexus_vm::{
+    instructions::Width,
+    memory::{paged::Paged, Memory},
+    syscalls::Syscalls,
+};
 
-use crate::error::{NexusVMError::InvalidInstruction, Result};
-use crate::instructions::{Inst, Opcode, Opcode::*, Width};
-use crate::memory::Memory;
-use crate::syscalls::Syscalls;
+use std::collections::HashSet;
 
-/// State of a running Nexus VM program.
+use crate::error::*;
+use crate::rv32::{parse::*, *};
+
+/// virtual machine state
 #[derive(Default)]
-pub struct NexusVM<M: Memory> {
-    /// Current program counter.
-    pub pc: u32,
-    /// Register file.
-    pub regs: [u32; 32],
-    /// Most recent instruction.
-    pub inst: Inst,
-    /// Result of most recent instruction.
-    pub Z: u32,
+pub struct VM {
+    /// ISA registers
+    pub regs: Regs,
     /// Syscall implementation
     pub syscalls: Syscalls,
-    /// Machine memory.
-    pub memory: M,
-    /// Memory proof for current instruction at pc
-    pub pc_proof: M::Proof,
-    /// Memory proof for load/store instructions.
-    pub read_proof: Option<M::Proof>,
-    /// Memory proof for store instructions.
-    pub write_proof: Option<M::Proof>,
+    /// machine memory
+    pub mem: Paged,
+    /// current instruction
+    pub inst: Inst,
+    /// internal result register
+    pub Z: u32,
+    /// used instruction sets
+    pub instruction_sets: HashSet<InstructionSet>,
 }
 
-/// Generate a trivial VM with a single NOP and a single HALT instruction.
-pub fn halt_vm<M: Memory>() -> NexusVM<M> {
-    let mut vm = NexusVM::<M>::default();
-    let inst = Inst { opcode: NOP, ..Inst::default() };
-    vm.memory.write_inst(vm.pc, inst.into()).unwrap();
-    let inst = Inst { opcode: HALT, ..Inst::default() };
-    vm.memory.write_inst(vm.pc + 8, inst.into()).unwrap();
-    vm
+/// ISA defined registers
+#[derive(Debug, PartialEq, Default)]
+pub struct Regs {
+    /// ISA defined program counter register
+    pub pc: u32,
+    /// ISA defined registers x0-x31
+    pub x: [u32; 32],
 }
 
-#[inline]
-fn add32(x: u32, y: u32) -> u32 {
-    x.overflowing_add(y).0
+impl VM {
+    pub fn new(pc: u32) -> Self {
+        let mut vm = Self::default();
+
+        vm.regs.pc = pc;
+        vm.instruction_sets = HashSet::new();
+
+        vm
+    }
+
+    /// get value of register r
+    pub fn get_reg(&self, r: u32) -> u32 {
+        if r == 0 {
+            0
+        } else {
+            self.regs.x[r as usize]
+        }
+    }
+
+    /// set value of register r
+    pub fn set_reg(&mut self, r: u32, val: u32) {
+        if r != 0 {
+            self.regs.x[r as usize] = val;
+        }
+    }
+
+    /// initialize memory from slice
+    pub fn init_memory(&mut self, addr: u32, bytes: &[u8]) -> Result<()> {
+        // slow, but simple
+        for (i, b) in bytes.iter().enumerate() {
+            self.mem.store(Width::B, addr + (i as u32), *b as u32)?;
+        }
+        Ok(())
+    }
 }
 
-#[inline]
-fn mul32(x: u32, y: u32) -> u32 {
-    x.overflowing_mul(y).0
+pub fn add32(a: u32, b: u32) -> u32 {
+    a.overflowing_add(b).0
 }
 
-#[inline]
-fn sub32(x: u32, y: u32) -> u32 {
-    x.overflowing_sub(y).0
+pub fn sub32(a: u32, b: u32) -> u32 {
+    a.overflowing_sub(b).0
 }
 
-// Evaluator for branch conditions.
-fn brcc(opcode: Opcode, x: u32, y: u32) -> bool {
-    match opcode {
+fn br_op(bop: BOP, x: u32, y: u32) -> bool {
+    match bop {
         BEQ => x == y,
         BNE => x != y,
         BLT => (x as i32) < (y as i32),
         BGE => (x as i32) >= (y as i32),
         BLTU => x < y,
         BGEU => x >= y,
-        _ => unreachable!(),
     }
 }
 
-/// Execute one step of a running Nexus VM.
-/// This function will load the next instruction at the address
-/// located at the program counter, execute the instruction,
-/// and update the register file, program counter, and merkle
-/// proofs.
-pub fn eval_step(vm: &mut NexusVM<impl Memory>) -> Result<()> {
-    let (dword, proof) = vm.memory.read_inst(vm.pc)?;
-    let Some(inst) = Inst::from_u64(dword) else {
-        return Err(InvalidInstruction(dword, vm.pc));
-    };
-
-    let I = inst.imm;
-    let X = vm.regs[inst.rs1 as usize];
-    let Y = vm.regs[inst.rs2 as usize];
-
-    let YI = add32(Y, I);
-    let shamt = YI & 0x1f;
-
-    let mut PC = 0u32;
-
-    vm.inst = inst;
-    vm.Z = 0;
-    vm.pc_proof = proof;
-    vm.read_proof = None;
-    vm.write_proof = None;
-
-    match inst.opcode {
-        NOP => {}
-        HALT => {
-            PC = vm.pc;
-        }
-        SYS => {
-            vm.Z = vm.syscalls.syscall(vm.pc, vm.regs, &vm.memory)?;
-        }
-        JAL => {
-            vm.Z = add32(vm.pc, 8);
-            let XI = add32(X, I);
-            // This semantics treats canonical call/ret
-            // differently from general jalr.
-            // TODO: seperate call/ret into their own instructions.
-            if inst.rs1 <= 1 {
-                PC = XI;
-            } else {
-                PC = mul32(XI, 2);
-            }
-        }
-        BEQ | BNE | BLT | BGE | BLTU | BGEU => {
-            if brcc(inst.opcode, X, Y) {
-                PC = add32(vm.pc, I);
-            }
-        }
-
-        LB | LH | LW | LBU | LHU => {
-            // Note: unwrap cannot fail
-            let width = Width::try_from(inst.opcode).unwrap();
-            let addr = add32(X, I);
-            let (val, proof) = vm.memory.load(width, addr)?;
-            vm.read_proof = Some(proof);
-            vm.Z = val;
-        }
-        SB | SH | SW => {
-            // Note: unwrap cannot fail
-            let width = Width::try_from(inst.opcode).unwrap();
-            let addr = add32(X, I);
-            let (_, proof) = vm.memory.load(width, addr)?;
-            vm.read_proof = Some(proof);
-            vm.write_proof = Some(vm.memory.store(width, addr, Y)?);
-        }
-
-        ADD => vm.Z = add32(X, YI),
-        SUB => vm.Z = sub32(X, YI),
-        SLT => vm.Z = ((X as i32) < (YI as i32)) as u32,
-        SLTU => vm.Z = (X < YI) as u32,
-        SLL => vm.Z = X << shamt,
-        SRL => vm.Z = X >> shamt,
-        SRA => vm.Z = ((X as i32) >> shamt) as u32,
-        AND => vm.Z = X & YI,
-        OR => vm.Z = X | YI,
-        XOR => vm.Z = X ^ YI,
+fn alu_op(aop: AOP, x: u32, y: u32) -> u32 {
+    let shamt = y & 0x1f;
+    match aop {
+        ADD => add32(x, y),
+        SUB => sub32(x, y),
+        SLT => ((x as i32) < (y as i32)) as u32,
+        SLTU => (x < y) as u32,
+        SLL => x << shamt,
+        SRL => x >> shamt,
+        SRA => ((x as i32) >> shamt) as u32,
+        AND => x & y,
+        OR => x | y,
+        XOR => x ^ y,
     }
+}
 
-    if inst.rd > 0 {
-        vm.regs[inst.rd as usize] = vm.Z;
+/// evaluate next instruction
+pub fn eval_inst(vm: &mut VM) -> Result<()> {
+    let slice = vm.mem.load(Width::W, vm.regs.pc)?.0.to_le_bytes();
+    vm.inst = parse_inst(vm.regs.pc, &slice)?;
+
+    // initialize micro-architecture state
+    vm.Z = 0;
+    let mut RD = 0u32;
+    let mut PC = 0;
+
+    vm.instruction_sets.insert(vm.inst.inst.instruction_set());
+
+    match vm.inst.inst {
+        LUI { rd, imm } => {
+            RD = rd;
+            vm.Z = imm;
+        }
+        AUIPC { rd, imm } => {
+            RD = rd;
+            vm.Z = add32(vm.regs.pc, imm);
+        }
+        JAL { rd, imm } => {
+            RD = rd;
+            vm.Z = add32(vm.regs.pc, 4);
+            PC = add32(vm.regs.pc, imm);
+        }
+        JALR { rd, rs1, imm } => {
+            let X = vm.get_reg(rs1);
+            RD = rd;
+            vm.Z = add32(vm.regs.pc, 4);
+            PC = add32(X, imm);
+        }
+        BR { bop, rs1, rs2, imm } => {
+            let X = vm.get_reg(rs1);
+            let Y = vm.get_reg(rs2);
+
+            if br_op(bop, X, Y) {
+                PC = add32(vm.regs.pc, imm);
+            }
+        }
+        LOAD { lop, rd, rs1, imm } => {
+            let X = vm.get_reg(rs1);
+            RD = rd;
+
+            let addr = add32(X, imm);
+            match lop {
+                LB => vm.Z = vm.mem.load(Width::B, addr)?.0,
+                LH => vm.Z = vm.mem.load(Width::H, addr)?.0,
+                LW => vm.Z = vm.mem.load(Width::W, addr)?.0,
+                LBU => vm.Z = vm.mem.load(Width::BU, addr)?.0,
+                LHU => vm.Z = vm.mem.load(Width::HU, addr)?.0,
+            }
+        }
+        STORE { sop, rs1, rs2, imm } => {
+            let X = vm.get_reg(rs1);
+            let Y = vm.get_reg(rs2);
+
+            let addr = add32(X, imm);
+            match sop {
+                SB => vm.mem.store(Width::B, addr, Y)?,
+                SH => vm.mem.store(Width::H, addr, Y)?,
+                SW => vm.mem.store(Width::W, addr, Y)?,
+            };
+        }
+        ALUI { aop, rd, rs1, imm } => {
+            RD = rd;
+            let X = vm.get_reg(rs1);
+            vm.Z = alu_op(aop, X, imm);
+        }
+        ALU { aop, rd, rs1, rs2 } => {
+            let X = vm.get_reg(rs1);
+            let Y = vm.get_reg(rs2);
+            RD = rd;
+            vm.Z = alu_op(aop, X, Y);
+        }
+        FENCE | EBREAK => {}
+        ECALL => {
+            vm.Z = vm.syscalls.syscall(vm.regs.pc, vm.regs.x, &vm.mem)?;
+        }
+        UNIMP => {
+            PC = vm.inst.pc;
+        }
     }
 
     if PC == 0 {
-        vm.pc = add32(vm.pc, 8);
-    } else {
-        vm.pc = PC;
+        PC = add32(vm.inst.pc, vm.inst.len);
     }
-
-    Ok(())
-}
-
-/// Run a VM to completion. The VM will stop when it encounters
-/// a HALT instruction.
-pub fn eval(vm: &mut NexusVM<impl Memory>, verbose: bool) -> Result<()> {
-    loop {
-        let pc = vm.pc;
-        eval_step(vm)?;
-        if verbose {
-            println!("{:x} {:?}", pc, vm.inst);
-        }
-        if vm.inst.opcode == HALT {
-            break;
-        }
-    }
+    vm.set_reg(RD, vm.Z);
+    vm.regs.pc = PC;
     Ok(())
 }
