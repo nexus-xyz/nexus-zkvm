538cab835559249d865714440056b1f5b34fc747
Sage Mitchell <sage@nexus.xyz>
2024-05-28 17:03:01 -0700

    Repro with #164 (19d7e45)

--- examples/src/bin/display_bug.rs
+++ examples/src/bin/display_bug.rs
@@ -11,30 +11,26 @@ struct Parens(u8);
 
 impl Display for Parens {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        write!(f, "~~({})~~", self.0)
+        write!(f, "({})", self.0)
     }
 }
 
 // Alternative implementation which apparently works around the problem:
 // impl Display for Parens {
 //     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-//         write!(f, "~~({}", self.0)?;
-//         write!(f, ")~~")
+//         write!(f, "({}", self.0)?;
+//         write!(f, ")")
 //     }
 // }
-
 
 #[nexus_rt::main]
 fn main() {
     let x = Parens(0);
     let y = x.to_string();
 
-    nexus_rt::print!("~~A~~\n");
     nexus_rt::print!("{}\n", x);
-    nexus_rt::print!("~~B~~\n");
     nexus_rt::print!("{}\n", y);
-    nexus_rt::print!("~~C~~\n");
 
     // assertion fails unless the workaround in the `Display` impl is used instead
-    assert_eq!(y, r#"~~(0)~~"#);
+    assert_eq!(y, r#"(0)"#);
 }
