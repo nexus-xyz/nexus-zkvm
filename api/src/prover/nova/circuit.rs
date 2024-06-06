7abe4db31cbcc85a8ef9217a0079f15f40a5e960
Samuel Judson <sam@sjudson.com>
2024-06-06 17:11:43 -0400

    Revert circuit.

--- api/src/prover/nova/circuit.rs
+++ api/src/prover/nova/circuit.rs
@@ -1,6 +1,4 @@
-use core::marker::PhantomData;
-use ark_ff::{BigInt, PrimeField};
-
+use ark_ff::BigInt;
 pub use ark_r1cs_std::{
     alloc::AllocVar,
     fields::{
@@ -11,22 +9,22 @@ pub use ark_r1cs_std::{
 };
 pub use ark_relations::{
     lc,
-    r1cs::{SynthesisError, SynthesisMode, Variable, ConstraintSystemRef},
+    r1cs::{SynthesisError, SynthesisMode, Variable},
 };
 
 use nexus_vm::{
     circuit::{build_constraints, ARITY},
     machines::nop_vm,
-    memory::{Memory, MemoryProof},
+    memory::{path::Path, trie::MerkleTrie},
     trace::{trace, Trace},
 };
-use nexus_nova::StepCircuit;
 
-use crate::prover::nova::error::*;
+use crate::error::*;
+use crate::types::*;
 
-pub struct Tr<F: PrimeField, P: MemoryProof>(pub Trace<P>, pub PhantomData<F>);
+pub struct Tr(pub Trace<Path>);
 
-impl<F: PrimeField, P: MemoryProof> Tr<F, P> {
+impl Tr {
     pub fn steps(&self) -> usize {
         self.0.blocks.len()
     }
@@ -35,26 +33,26 @@ impl<F: PrimeField, P: MemoryProof> Tr<F, P> {
         self.0.k * self.0.blocks.len()
     }
 
-    pub fn input(&self, index: usize) -> Result<Vec<F>, ProofError> {
+    pub fn input(&self, index: usize) -> Result<Vec<F1>, ProofError> {
         self.0.input(index).ok_or(ProofError::InvalidIndex(index))
     }
 }
 
-pub fn nop_circuit<F: PrimeField, M: Memory>(k: usize) -> Result<Tr<F, M::Proof>, ProofError> {
-    let mut vm = nop_vm::<M>(1);
+pub fn nop_circuit(k: usize) -> Result<Tr, ProofError> {
+    let mut vm = nop_vm::<MerkleTrie>(1);
     let trace = trace(&mut vm, k, false)?;
-    Ok(Tr(trace, PhantomData))
+    Ok(Tr(trace))
 }
 
-impl<F: PrimeField, P: MemoryProof + Send> StepCircuit<F> for Tr<F, P> {
+impl StepCircuit<F1> for Tr {
     const ARITY: usize = ARITY;
 
     fn generate_constraints(
         &self,
-        cs: ConstraintSystemRef<F>,
-        k: &FpVar<F>,
-        z: &[FpVar<F>],
-    ) -> Result<Vec<FpVar<F>>, SynthesisError> {
+        cs: CS,
+        k: &FpVar<F1>,
+        z: &[FpVar<F1>],
+    ) -> Result<Vec<FpVar<F1>>, SynthesisError> {
         let index = k.value().map_or(0, |s| match s.into_bigint() {
             BigInt(l) => l[0] as usize,
         });
