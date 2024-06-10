29bd92dc996bd45112b581b7a09e56df6b9f88e4
Samuel Judson <sam@sjudson.com>
2024-06-10 10:29:45 -0400

    Format.

--- api/examples/prover_run.rs
+++ api/examples/prover_run.rs
@@ -40,7 +40,8 @@ fn main() {
     println!("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
 
     println!("Proving execution of length {}...", trace.blocks.len());
-    let proof = nexus_api::prover::nova::prove_seq(&public_params, trace).expect("error proving execution");
+    let proof =
+        nexus_api::prover::nova::prove_seq(&public_params, trace).expect("error proving execution");
 
     print!("Verifying execution...");
     proof
