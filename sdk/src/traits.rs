bd65d8b1ecd49a4d5273013abd26a465e3b00e20
Samuel Judson <sam@sjudson.com>
2024-07-01 16:48:43 -0400

    Start to scratch out a redesign.

--- sdk/src/traits.rs
+++ sdk/src/traits.rs
@@ -1,41 +1,35 @@
 use std::path::Path;
 
-pub trait ExecutionState {}
+pub(crate) trait ExecutionState {}
+pub(crate) trait Compute {}
 
-pub enum Uninitialized {}
-impl ExecutionState for Uninitialized {}
-
-pub enum Initialized {}
-impl ExecutionState for Initialized {}
-
-pub enum Traced {}
-impl ExecutionState for Traced {}
-
-pub trait Compute {}
-
-pub enum Local {}
-impl Compute for Local {}
-
-//enum Cloud {}
-//impl Cloud for Compute {}
-
-pub trait Prover<S: ExecutionState = Uninitialized, C: Compute = Local> {
+pub trait Prover {
     type Memory;
     type Params;
     type Proof;
     type Error;
-    
-    fn gen_pp() -> Result<Self::Params, Self::Error>;
+}
 
-    fn load_pp(path: AsRef<Path>) -> Result<Self::Params, Self::Error>;
-    
-    fn save_pp(pp: &Self::Params, path: AsRef<Path>) -> Result<(), Self::Error>;
+pub trait Configurable {
+    type Memory;
+    type Error;
 
     fn new(elf_bytes: &[u8]) -> Self;
-
     fn new_from_file(path: AsRef<Path>) -> Self;
+}
+
+pub trait Executable {
+    type Memory;
+    type Error;
 
     fn trace<T>(self, input: Option<T>) -> Self;
+}
+
+pub trait Provable {
+    type Memory;
+    type Params;
+    type Proof;
+    type Error;
 
     fn prove<T>(self, pp: &Self::Params, input: Option<T>) -> Result<Self::Proof, Self::Error>;
 }
@@ -43,6 +37,12 @@ pub trait Prover<S: ExecutionState = Uninitialized, C: Compute = Local> {
 pub trait Verifiable {
     type Params;
     type Error;
+
+    fn gen_pp() -> Result<Self::Params, Self::Error>;
+
+    fn load_pp(path: AsRef<Path>) -> Result<Self::Params, Self::Error>;
+
+    fn save_pp(pp: &Self::Params, path: AsRef<Path>) -> Result<(), Self::Error>;
 
     fn verify(&self) -> Result<(), Self::Error>;
 }
