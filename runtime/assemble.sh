61a616ea0f7556b989705829b2da5d26657dacf4
duc-nx <>
2024-07-25 06:08:23 -0400

    Rename assemble.sh tool
    Most of us use Mac.
    I suggest change the command to native mactool.

--- runtime/assemble.sh
+++ runtime/assemble.sh
@@ -6,8 +6,8 @@ rm -f bin/*.a
 
 for ext in i imc
 do
-    riscv-none-elf-gcc -c -mabi=ilp32 -march=rv32${ext} -mcmodel=medlow asm.S -o bin/nexus-rt.o
-    riscv-none-elf-ar crs bin/riscv32${ext}-unknown-none-elf.a bin/nexus-rt.o
+    riscv64-elf-gcc -c -mabi=ilp32 -march=rv32${ext} -mcmodel=medlow asm.S -o bin/nexus-rt.o
+    riscv64-elf-ar crs bin/riscv32${ext}-unknown-none-elf.a bin/nexus-rt.o
 done
 
 rm bin/nexus-rt.o
