738192052b7a62eb4ece482176528299ce974fd9
duc-nx <>
2024-07-31 14:21:27 -0400

    Refactor test_sdk.sh
    - Modify guest Cargo.toml link to current commit runtime
    - Restructure script in function.

--- assets/scripts/test_sdk.sh
+++ assets/scripts/test_sdk.sh
@@ -1,59 +1,78 @@
 #!/bin/bash
-# This script follows steps written in README.md
-# Using the rust file specified as the unique argument.
-# Call at the top directory of the nexus-zkvm project
-# For example:
-# ./assets/scripts/test_sdk.sh examples/src/bin/fib3.rs
-# Every command needs to succeed
 set -e
 
 ORIGINAL_DIR=$(pwd)
 PROJECT_NAME="/tmp/nexus-host-ci"
 
-if [ "$#" -ne 1 ]; then
+function usage() {
     echo "Usage: $0 <file.rs>"
+    echo "where <file.rs> is a path to an existing Rust file."
     exit 1
-fi
+}
 
-if [ ! -f "$1" ]; then
-    echo "Usage: $0 <file.rs>"
-    echo "where <file.rs> is a path to a file."
-    exit 1
-fi
-
-if [ -e "$PROJECT_NAME" ]; then
-    echo "Error: Directory '$PROJECT_NAME' already exists."
-    exit 1
-fi
-
-set -x
-
-cleanup() {
+function cleanup() {
     cd "$ORIGINAL_DIR"
     rm -rf "$PROJECT_NAME"
 }
 
-trap cleanup SIGINT
-
-error_handler() {
+function error_handler() {
     echo "Error occurred in test_sdk.sh on line ${1}. Exiting."
     cleanup
     exit 1
 }
 
-trap 'error_handler $LINENO' ERR
+function check_arguments() {
+    if [ "$#" -ne 1 ] || [ ! -f "$1" ]; then
+        usage
+    fi
+}
 
-# Builds current cargo-nexus tool from current commit
+function check_project_directory() {
+    if [ -e "$PROJECT_NAME" ]; then
+        echo "Error: Directory '$PROJECT_NAME' already exists."
+        exit 1
+    fi
+}
+
+function build_cargo_nexus() {
 cargo build --release --package cargo-nexus --bin cargo-nexus
+}
+
+function create_nexus_project() {
 ./target/release/cargo-nexus nexus host "$PROJECT_NAME"
-# Copy the test source file to guest program
+}
+
+function copy_test_file() {
 cp "$1" "$PROJECT_NAME/src/guest/src/main.rs"
-cd "$PROJECT_NAME"
+}
 
-# Link the test program to the current sdk commit
-sed -e "s#nexus-sdk = { git = \"https://github.com/nexus-xyz/nexus-zkvm.git\", version = \"0.2.0\" }#nexus-sdk = { path = \"$ORIGINAL_DIR/sdk\"}#" Cargo.toml > Cargo.tmp && mv Cargo.tmp Cargo.toml
+function update_dependencies() {
+    # Link host SDK in Cargo.toml to the current SDK commit
+    sed -i.bak "s#nexus-sdk = { git = \"https://github.com/nexus-xyz/nexus-zkvm.git\", version = \"0.2.0\" }#nexus-sdk = { path = \"$ORIGINAL_DIR/sdk\"}#" Cargo.toml
+    cd src/guest
+    # Link guest runtime in Cargo.toml to the current commit runtime
+    sed -i.bak "s#nexus-rt = { git = \"https://github.com/nexus-xyz/nexus-zkvm.git\", version = \"0.2.0\" }#nexus-rt = { path = \"$ORIGINAL_DIR/runtime\" }#" Cargo.toml
+    cd ../../
+}
 
+function run_project() {
 cargo update
 cargo run --release
+}
+
+trap cleanup SIGINT
+trap 'error_handler $LINENO' ERR
+
+check_arguments "$@"
+check_project_directory
+
+set -x
+
+build_cargo_nexus
+create_nexus_project
+copy_test_file "$1"
+cd "$PROJECT_NAME"
+update_dependencies
+run_project
 
 cleanup
