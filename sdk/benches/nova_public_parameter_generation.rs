4de02664df27c71f900ddf2b753e18bfac8eb0de
duc-nx <>
2024-08-08 15:24:17 -0400

    Add Nova Public Parameter benchmark for SDK
    Summary:
    
    Test Plan:

---
+++ sdk/benches/nova_public_parameter_generation.rs
@@ -0,0 +1,20 @@
+use divan;
+use divan::{black_box, AllocProfiler};
+use nexus_sdk::nova;
+use nexus_sdk::Parameters;
+
+#[global_allocator]
+static ALLOC: AllocProfiler = AllocProfiler::system();
+
+fn nova_public_parameter_generation(k: usize) {
+    nova::seq::PP::generate_for_testing(k).expect("Failed to generate Nova public parameters");
+}
+
+#[divan::bench(args = [1, 2, 4], max_time = 10)]
+fn bench_nova_public_parameter(k: usize) {
+    black_box(nova_public_parameter_generation(k));
+}
+
+fn main() {
+    divan::main();
+}
