785c265c5c28aaf331cb0f36f6641ff3856e1665
Samuel Judson <sam@sjudson.com>
2024-08-15 11:12:37 -0400

    Another minor.

--- sdk/examples/nova_build.rs
+++ sdk/examples/nova_build.rs
@@ -7,14 +7,14 @@ use nexus_sdk::{
 const PACKAGE: &str = "example";
 
 fn main() {
-    println!("Setting up Nova public parameters...");
-    let pp: PP = PP::generate().expect("failed to generate parameters");
-
     let mut opts = CompileOpts::new(PACKAGE);
     opts.set_memlimit(8); // use an 8mb memory
 
     println!("Compiling guest program...");
     let prover: Nova<Local> = Nova::compile(&opts).expect("failed to compile guest program");
+
+    println!("Setting up Nova public parameters...");
+    let pp: PP = PP::generate().expect("failed to generate parameters");
 
     println!("Proving execution of vm...");
     let proof = prover.prove(&pp).expect("failed to prove program");
