cb982976fad8330907930158be7e01ed76e0c4b2
Yoichi Hirai <yoichi@nexus.xyz>
2024-11-20 19:44:13 +0000

    feat: fill OpC and ImmC columns (#185)

--- prover/src/machine2/chips/cpu.rs
+++ prover/src/machine2/chips/cpu.rs
@@ -8,7 +8,10 @@ use crate::machine2::{
     traits::MachineChip,
 };
 
-use nexus_vm::riscv::BuiltinOpcode;
+use nexus_vm::riscv::{
+    BuiltinOpcode,
+    InstructionType::{BType, IType, ITypeShamt, JType, RType, SType, UType, Unimpl},
+};
 
 pub struct CpuChip;
 
@@ -63,6 +66,23 @@ impl MachineChip for CpuChip {
         // Fill OpB to the main trace
         let op_b = vm_step.step.instruction.op_b as u8;
         traces.fill_columns(row_idx, &[op_b], OpB);
+
+        // Fill OpC (if register index) or ImmC (if immediate) to the main trace
+        let op_c_raw = vm_step.step.instruction.op_c;
+        match vm_step.step.instruction.ins_type {
+            RType => {
+                traces.fill_columns(row_idx, &[op_c_raw as u8], OpC);
+            }
+            IType | BType | SType | ITypeShamt | JType | UType => {
+                traces.fill_columns(row_idx, &[1], ImmC); // ImmC is a boolean flag
+            }
+            Unimpl => {
+                panic!(
+                    "Unsupported instruction type: {:?}",
+                    vm_step.step.instruction.ins_type
+                );
+            }
+        }
 
         // Fill ValueAEffectiveFlag to the main trace
         let value_a_effective_flag = if op_a == 0 { 0 } else { 1 };
