9b113d5ae10ec510e8316c468f07bf24d7bcc619
MozirDmitriy <dmitriymozir@gmail.com>
2025-10-08 17:01:57 +0300

    Remove unused HalfWord virtual column and re-export from CPU component

--- prover2/machine/src/components/cpu/columns.rs
+++ prover2/machine/src/components/cpu/columns.rs
@@ -1,12 +1,7 @@
-use nexus_common::constants::{WORD_SIZE, WORD_SIZE_HALVED};
-use stwo::{core::fields::m31::BaseField, prover::backend::simd::m31::PackedBaseField};
+use stwo::core::fields::m31::BaseField;
 
 use nexus_vm_prover_air_column::{AirColumn, PreprocessedAirColumn};
-use nexus_vm_prover_trace::{
-    component::{ComponentTrace, FinalizedColumn},
-    eval::TraceEval,
-    virtual_column::VirtualColumn,
-};
+use nexus_vm_prover_trace::eval::TraceEval;
 use stwo_constraint_framework::EvalAtRow;
 
 #[derive(Debug, Copy, Clone, PreprocessedAirColumn)]
@@ -33,27 +28,4 @@ pub enum Column {
     IsPad,
 }
 
-/// Two limbs combined into a single 16 bit column.
-pub struct HalfWord<C> {
-    pub col: C,
-    pub idx: usize,
-}
 
-impl<C: AirColumn> VirtualColumn for HalfWord<C> {
-    type Column = C;
-
-    fn eval<E: EvalAtRow, P: PreprocessedAirColumn>(
-        &self,
-        trace_eval: &TraceEval<P, Self::Column, E>,
-    ) -> E::F {
-        let word: [E::F; WORD_SIZE] = trace_eval.column_eval(self.col);
-        word[self.idx * WORD_SIZE_HALVED].clone()
-            + word[self.idx * WORD_SIZE_HALVED + 1].clone() * BaseField::from(1 << 8)
-    }
-
-    fn combine_at_row(&self, component_trace: &ComponentTrace, vec_idx: usize) -> PackedBaseField {
-        let word: [FinalizedColumn<'_>; WORD_SIZE] = component_trace.original_base_column(self.col);
-        word[self.idx * WORD_SIZE_HALVED].at(vec_idx)
-            + word[self.idx * WORD_SIZE_HALVED + 1].at(vec_idx) * BaseField::from(1 << 8)
-    }
-}
